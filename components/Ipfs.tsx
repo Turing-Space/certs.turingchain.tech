c5a10cebec2d77426c68221e6068b6aaea0b7064
TseHang <a54383813@gmail.com>
2019-12-05 17:22:19 +0900

    improve

--- components/Ipfs.tsx
+++ components/Ipfs.tsx
@@ -1,5 +1,5 @@
 import { NextFC } from 'next';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 import styled from 'styled-components';
 import { getRelativePath } from '@/utils';
@@ -10,50 +10,59 @@ import Side from '@/components/verifier/Proof';
 import Certificate from '@/components/verifier/Certificate';
 
 function timeConverter(UNIX_timestamp: number) {
-  var a = new Date(UNIX_timestamp * 600);
-  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  var year = a.getFullYear();
-  var month = months[a.getMonth()];
-  var date = a.getDate();
-  var hour = a.getHours();
-  var min = a.getMinutes();
-  var sec = a.getSeconds();
-  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+  const a = new Date(UNIX_timestamp * 600);
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const year = a.getFullYear();
+  const month = months[a.getMonth()];
+  const date = a.getDate();
+  const hour = a.getHours();
+  const min = a.getMinutes();
+  const sec = a.getSeconds();
+  const time =
+    date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
   return time;
 }
 
-let Root = styled.div`
+const Root = styled.div<{ zoomLevel: number }>`
   width: 100%;
   height: 100%;
   background: $fafafa;
-`
+  zoom: ${p => p.zoomLevel};
+`;
 
 const Ipfs: NextFC = () => {
-  const [date, setDate] = useState("-");
-  const [issuerName, setIssuerName] = useState("-");
-  const [type, setType] = useState("-");
-  const [holderName, setHolderName] = useState("-");
-  const [holderEmail, setEmail] = useState("-");
-  const [IPFS, setIPFS] = useState("-");
-  const [IOTA, setIOTA] = useState("-");
-  const [zoomLevel, setZoomLevel] = useState(0.7);
-  const [width, setWidth] = useState(1920);
-  const [height, setHeight] = useState(1170);
+  const [date, setDate] = useState('-');
+  const [issuerName, setIssuerName] = useState('-');
+  const [type, setType] = useState('-');
+  const [holderName, setHolderName] = useState('-');
+  const [holderEmail, setEmail] = useState('-');
+  const [IPFS, setIPFS] = useState('-');
+  const [IOTA, setIOTA] = useState('-');
+  const [zoomLevel, setZoomLevel] = useState(0);
 
   useEffect(() => {
-    if (window.innerWidth > 600)
-      setZoomLevel(Math.min(window.innerWidth / 1920, window.innerHeight / 1170))
-    else
-      setZoomLevel(window.innerWidth / 480)
-
     async function fetchCertsAPI() {
       const value = queryString.parse(window.location.search);
       const hash = value.hash;
       console.log(hash);
-      await fetch(`https://x1certificate-aqkcbxdduq-uc.a.run.app/v1/view/certs?ipfs=${hash}`)
+      await fetch(
+        `https://x1certificate-aqkcbxdduq-uc.a.run.app/v1/view/certs?ipfs=${hash}`,
+      )
         .then(res => res.json())
-        .then((returnData) => {
-          console.log(returnData);
+        .then(returnData => {
           setDate(timeConverter(returnData.content.timestamp));
           setIssuerName(returnData.content.issuerName);
           setType(returnData.content.type);
@@ -62,36 +71,31 @@ const Ipfs: NextFC = () => {
           setHolderName(returnData.content.holderName);
           setEmail(returnData.content.emailHolder);
         })
-        .catch(console.log)
-    };
-    fetchCertsAPI();
-
-    function handleResize() {
-      setWidth(window.innerWidth)
-      setHeight(window.innerHeight)
-      if (window.innerWidth > 600)
-        setZoomLevel(Math.min(window.innerWidth / 1920, window.innerHeight / 1170))
-      else
-        setZoomLevel(window.innerWidth / 480)
+        .catch(console.error);
     }
-
-    window.addEventListener('resize', handleResize)
-
+    fetchCertsAPI();
   }, []);
 
-  let RootStyle
-  if (width > 600) {
-    RootStyle = {
-      zoom: zoomLevel
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth > 600) {
+        setZoomLevel(
+          Math.min(window.innerWidth / 1920, window.innerHeight / 1170),
+        );
+      } else {
+        setZoomLevel(window.innerWidth / 480);
+      }
     }
-  }
-  else {
-    RootStyle = {
-      zoom: width / 480
-    }
-  }
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <Root style={RootStyle}>
+    <Root zoomLevel={zoomLevel}>
       <Head>
         <title>TuringVerifier</title>
         <link
@@ -100,17 +104,26 @@ const Ipfs: NextFC = () => {
           href={getRelativePath('/static/css/ipfs.css')}
         />
       </Head>
-      <Side date={date}
-        issuerName={issuerName}
-        type={type}
-        holderName={holderName}
-        holderEmail={holderEmail}
-        ipfs={IPFS}
-        iota={IOTA} />
-      <Certificate ipfs={IPFS} />
-    </Root >
-  )
-
-}
+      {zoomLevel === 0 ? (
+        <p style={{ width: '100%', height: '100px', textAlign: 'center' }}>
+          資料載入中...
+        </p>
+      ) : (
+        <>
+          <Side
+            date={date}
+            issuerName={issuerName}
+            type={type}
+            holderName={holderName}
+            holderEmail={holderEmail}
+            ipfs={IPFS}
+            iota={IOTA}
+          />
+          <Certificate ipfs={IPFS} />
+        </>
+      )}
+    </Root>
+  );
+};
 
 export default Ipfs;
--- components/verifier/Certificate.tsx
+++ components/verifier/Certificate.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { FC } from 'react';
+import React, { FC } from 'react';
 
 import styled from 'styled-components';
 import PDFViewer from 'mgr-pdf-viewer-react';
@@ -12,25 +11,29 @@ const shareIcon = '/static/icon/icon-share-grey.svg';
 const Root = styled.div`
   display: inline-block;
   vertical-align: top;
-`
+`;
 const TitleBar = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
   height: 100px;
-`
+`;
 const TitleInfo = styled.div`
   display: flex;
   align-items: center;
   margin-left: 60px;
   margin-right: 60px;
-  width: 360px; //
+  width: 100vw;
   height: 100px;
   border-bottom: 1px solid #bdbdbd;
-`
+
+  @media (max-width: 600px) {
+    width: 360px;
+  }
+`;
 const Logo = styled.img`
   padding-right: 16px;
-`
+`;
 const Title = styled.div`
   font-family: ${p => p.theme.fontFamily.SFProText};
   font-size: 24px;
@@ -38,60 +41,62 @@ const Title = styled.div`
   color: #a80100;
   color: #a80100;
   align-items: right;
-  display: flex; 
+  display: flex;
   padding-right: 150px;
-`
-let PDF = styled.div`
-  padding-top: 50px;  
-  display: flex; 
+`;
+const PDF = styled.div`
+  padding-top: 50px;
+  display: flex;
   margin-left: 60px;
   padding-bottom: 60px;
-`
+`;
 type TProps = {
-    ipfs: string
+  ipfs: string;
 };
 
 const Certificate: FC<TProps> = props => {
-    return (
-        <Root>
-            <TitleBar>
-                <TitleInfo style={{ width: window.innerWidth > 600 ? window.innerWidth + 60 : 360 }}>
-                    <Logo src={getRelativePath(logoIcon)} />
-                    <a href={'http://certs.turingchain.tech'}>
-                        <Title>TuringCerts</Title>
-                    </a>
-                    {/* <a href={'https://ipfs.io/ipfs/' + props.ipfs} target="_blank" rel="noopener noreferrer">
+  return (
+    <Root>
+      <TitleBar>
+        <TitleInfo>
+          <Logo src={getRelativePath(logoIcon)} />
+          <a href={'http://certs.turingchain.tech'}>
+            <Title>TuringCerts</Title>
+          </a>
+          {/* <a href={'https://ipfs.io/ipfs/' + props.ipfs} target="_blank" rel="noopener noreferrer">
                         <img src={getRelativePath(shareIcon)} alt="" style={{ paddingLeft: '1000px' }} />
                     </a> */}
-                </TitleInfo>
-            </TitleBar>
-            {PDFF(props)}
-        </Root>
-    );
-}
+        </TitleInfo>
+      </TitleBar>
+      {PDFF(props)}
+    </Root>
+  );
+};
 
 const PDFF: FC<TProps> = props => {
+  if (props.ipfs !== '-') {
+    return (
+      <div>
+        <PDF>
+          <a href={'https://ipfs.io/ipfs/' + props.ipfs}>
+            <PDFViewer
+              document={{
+                url: 'https://ipfs.io/ipfs/' + props.ipfs,
+              }}
+              hideNavbar
+              scale={
+                window.innerWidth > 600
+                  ? window.innerWidth / 1200
+                  : window.innerWidth / (1152 + 630)
+              }
+            />
+          </a>
+        </PDF>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+};
 
-    if (props.ipfs !== "-") {
-        return (
-            <div>
-                <PDF>
-                    <a href={'https://ipfs.io/ipfs/' + props.ipfs}>
-                        <PDFViewer document={{
-                            url: 'https://ipfs.io/ipfs/' + props.ipfs
-                        }}
-                            hideNavbar
-                            scale={window.innerWidth > 600 ? window.innerWidth / 1200 : window.innerWidth / (1152 + 630)}
-                        />
-                    </a>
-                </PDF >
-            </div >
-        );
-    } else {
-        return (
-            <div></div>
-        );
-    }
-}
-
-export default Certificate
+export default Certificate;
