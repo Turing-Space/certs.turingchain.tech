12a575fb2e8a1d66b936ab49795e9c7baec1b8ed
tina1998612 <tina26919742@gmail.com>
2019-12-16 18:46:27 +0800

    add cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const compression = require('compression');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
+const cors = require('cors');
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const nextI18NextMiddleware = require('next-i18next/middleware').default;
@@ -13,6 +14,7 @@ app.prepare().then(() => {
   const server = express();
   server.use(compression());
   server.use(nextI18NextMiddleware(nextI18next));
+  server.use(cors());
   server.get('*', (req, res) => {
     return handle(req, res);
   });
