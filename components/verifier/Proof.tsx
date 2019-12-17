4974d1f15733bcac126805f6a2bb802536d07751
tina1998612 <tina26919742@gmail.com>
2019-12-17 14:32:45 +0800

    add cors

--- components/verifier/Proof.tsx
+++ components/verifier/Proof.tsx
@@ -142,7 +142,7 @@ const Proof: FC<TProps> = props => {
         <InfoContent>{props.holderEmail}</InfoContent>
         <InfoH1>Blockchain Footprint</InfoH1>
         <InfoH2>IPFS</InfoH2>
-        <a href={'http://ipfs.turingchain.tech/ipfs/' + props.ipfs}>
+        <a href={'https://ipfs.io/ipfs/' + props.ipfs}>
           <InfoContent>{props.ipfs}</InfoContent>
         </a>
         <InfoH2>IOTA</InfoH2>
