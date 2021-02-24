b88977fc989b853b986e94370f86ed5dee6ff88c
hachimanhiki <mh512158475@gmail.com>
2021-02-25 00:41:25 +0800

    feat: add privacy page

---
+++ pages/privacy.tsx
@@ -0,0 +1,18 @@
+import { NextFC } from 'next';
+import HomeLayout from '@/layouts/Home';
+import Privacy from '@/components/Privacy';
+import { SITE_TITLE, i18nNamespace } from '@/constants';
+
+const Index: NextFC = () => {
+  return (
+    <HomeLayout title={'TuringCerts | ' + SITE_TITLE} backgroundIsGrey={true}>
+      <Privacy />
+    </HomeLayout>
+  );
+};
+
+Index.getInitialProps = async () => ({
+  namespacesRequired: [i18nNamespace.Common, i18nNamespace.Home],
+});
+
+export default Index;
