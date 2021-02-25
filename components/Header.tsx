8a21225bf5c7c3efa0b1186b47e5cec48883ca62
hachimanhiki <mh512158475@gmail.com>
2021-02-26 02:21:07 +0800

    revert: remove section-news temporary

--- components/Header.tsx
+++ components/Header.tsx
@@ -185,7 +185,9 @@ const Header: SFC = () => {
           <li onClick={() => scrollToID('section-awards')}>
             {t('header.awards')}
           </li>
+          {/*
           <li onClick={() => scrollToID('section-news')}>{t('header.news')}</li>
+          */}
           {/*
           <li onClick={() => scrollToID('section-product')}>
             {t('header.product')}
