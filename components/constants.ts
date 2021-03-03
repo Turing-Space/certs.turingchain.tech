9b978a898d10957748ca2bfb6af4056bd41c5eab
paul <paul71633@gmail.com>
2021-03-03 17:24:44 +0800

    fix: delete redundant code in constants.ts

--- components/constants.ts
+++ components/constants.ts
@@ -1,6 +1,5 @@
 import { getRelativePath } from '@/utils';
 
-/* head meta tag data */
 export const SITE_TITLE = 'Turing Chain';
 export const SITE_DESC =
   '區塊鏈成就履歷。不可篡改、無國界的優勢，重新定義傳統教育，創造穩定永續的學習旅程追蹤，強化官方證書的最高信任與真實價值。';
@@ -21,7 +20,6 @@ export enum i18nNamespace {
 export const GA_ID = process.env.GA_ID;
 
 export const API_ENDPOINT = 'https://x1certificate-aqkcbxdduq-uc.a.run.app/v1';
-// export const API_ENDPOINT = 'http://localhost:4000/v1';
 
 export const langsMap: { [key: string]: boolean } = {
   'zh-TW': true,
