5c9e0e3c2cdf941fca9b6721468f0e0cf79377c7
simon824695173 <s3587378@gmail.com>
2021-03-10 10:51:04 +0800

    Adjust News page, About us. Add news page back

--- components/home/news/Carousel.tsx
+++ components/home/news/Carousel.tsx
@@ -11,11 +11,28 @@ import { useTranslation } from 'react-i18next';
 type NewsData = {
   source: string;
   link: string;
+  text: string;
 };
+
+const CarouselContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 80vh;
+  justify-content: center;
+`;
+
+const Button = styled.div`
+  z-index: 10;
+  width: 10vw;
+  background-color: transparent;
+  cursor: pointer;
+  display: flex;
+  user-select: none;
+`;
 
 const CardSlider = styled.div`
   position: relative;
-  height: 60vh;
+  height: inherit;
   width: 80vw;
   display: flex;
   flex-direction: column;
@@ -28,92 +45,111 @@ const CardSliderWrapper = styled.div`
   overflow-x: visible;
   overflow-y: hidden;
   display: flex;
-  height: 60vh;
+  height: inherit;
   left: 50%;
   align-items: center;
   justify-content: center;
 `;
 
 const cardProp = {
-  width: 300,
-  height: 200,
+  width: 25,
+  widthUnit: 'vw',
 };
 
+const Text = styled.div`
+  will-change: transform;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  padding: 0.75em 1em;
+  color: ${p => p.theme.colors.white};
+  background: rgba(0, 0, 0, 0.8);
+  font-size: ${p => p.theme.fontSize.smaller};
+  transform: translate3d(0, 100%, 0);
+  transition: transform 0.2s ease-in;
+`;
+
 const Card = styled.div`
-  width: ${cardProp.width}px;
-  height: ${cardProp.height}px;
+  width: ${cardProp.width.toString() + cardProp.widthUnit};
+  height: fit-content;
   transition: transform 300ms linear;
+  overflow: hidden;
+  border-radius: 3vh;
+  &:hover {
+    ${Text} {
+      transform: translate3d(0, 0, 0);
+    }
+  }
 `;
 
 const NewsImage = styled.img`
   width: 100%;
-  height: 100%;
-  border-radius: 10%;
-  object-fit: contain;
 `;
 
 const cards: NewsData[] = [
   {
-    source: getRelativePath('/static/certificate/Examples_Certificate1.png'),
-    link: 'https://www.youtube.com/?gl=TW&hl=zh-TW',
+    source: getRelativePath('/static/news/news_1.jpg'),
+    link: 'https://meet.bnext.com.tw/articles/view/46687',
+    text:
+      '大學四年都在連續創業！24歲胡耀傑打造圖靈鏈走上區塊鏈應用的冒險之路｜Meet 創業小聚 20200728',
   },
   {
-    source: getRelativePath('/static/certificate/Examples_Certificate2.png'),
-    link: 'https://www.google.com/?hl=zh_tw',
+    source: getRelativePath('/static/news/news_2.jpg'),
+    link: 'https://www.bnext.com.tw/article/60313/fiti0',
+    text:
+      '創新創業激勵計畫FITI引路 點亮學研團隊的創業路｜數位時代 Meet Taipei 20201203',
   },
   {
-    source: getRelativePath('/static/certificate/Examples_Certificate3.png'),
-    link: 'https://www.google.com/?hl=zh_tw',
+    source: getRelativePath('/static/news/news_3.jpg'),
+    link:
+      'https://blockcast.it/2020/11/25/iii-digital-education-badi-ai-fintech-asso-and-turing-chain-join-force-to-form-taiwan-e-portfolio-alliance/',
+    text:
+      '資策會攜圖靈鏈、全國商業總會及 AI 金融科技協會共組「臺灣學習履歷聯盟」藉防偽認證技術推廣區塊鏈證書｜區塊客 20201125',
   },
   {
-    source: getRelativePath('/static/certificate/Examples_Certificate4.png'),
-    link: 'https://www.google.com/?hl=zh_tw',
-  },
-  {
-    source: getRelativePath('/static/certificate/Examples_Certificate5.png'),
-    link: 'https://www.google.com/?hl=zh_tw',
-  },
-  {
-    source: getRelativePath('/static/certificate/Examples_Certificate6.png'),
-    link: 'https://www.google.com/?hl=zh_tw',
+    source: getRelativePath('/static/news/news_4.jpeg'),
+    link: 'https://becomingaces.com/article/161',
+    text:
+      '19世紀發明電話、20世紀造飛機、那21世紀呢？圖靈證書發明者胡耀傑：用科技打造更好的未來社會！｜未來大人物 20201229',
   },
 ];
 
 const Carousel: FC<{ newsNumber: number }> = ({ newsNumber }) => {
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(1);
   function previousNews() {
     if (index > 0) {
       setIndex(prevIndex => prevIndex - 1);
+    } else if (index === 0) {
+      setIndex(newsNumber - 1);
     }
   }
   function nextNews() {
     if (index < newsNumber - 1) {
       setIndex(prevIndex => prevIndex + 1);
+    } else if (index === newsNumber - 1) {
+      setIndex(0);
     }
   }
 
   return (
-    <div>
-      <button
+    <CarouselContainer>
+      <Button
         onClick={() => {
           previousNews();
         }}
       >
-        Previous
-      </button>
-      <button
-        onClick={() => {
-          nextNews();
-        }}
-      >
-        Next
-      </button>
+        <img
+          src={getRelativePath('/static/news/icon_left.svg')}
+          style={{ margin: 'auto' }}
+        />
+      </Button>
       <CardSlider>
         <CardSliderWrapper
           style={{
             transform: `translateX(${-((index + 1.5) * 100) /
               (newsNumber + 2)}%)`,
-            width: `${cardProp.width * (newsNumber + 2)}px`,
+            width: `${cardProp.width * (newsNumber + 2)}${cardProp.widthUnit}`,
           }}
         >
           {cards.map((property, idx) => {
@@ -126,20 +162,27 @@ const Carousel: FC<{ newsNumber: number }> = ({ newsNumber }) => {
             if (idx === index - 1 || idx === index || idx === index + 1) {
               return (
                 <Card
+                  key={property.link}
                   className={`news-card-${idx}`}
                   style={{
                     transform: `scale(${ratio})`,
                     zIndex: zIndex,
                   }}
                 >
-                  <a href={property.link} target="_blank">
+                  <a
+                    href={property.link}
+                    target="_blank"
+                    style={{ display: 'block' }}
+                  >
                     <NewsImage src={property.source} />
                   </a>
+                  <Text>{cards[idx].text}</Text>
                 </Card>
               );
             } else {
               return (
                 <Card
+                  key={property.link}
                   className={`news-card-${idx}`}
                   style={{ transform: `scale(${ratio})` }}
                 ></Card>
@@ -148,7 +191,17 @@ const Carousel: FC<{ newsNumber: number }> = ({ newsNumber }) => {
           })}
         </CardSliderWrapper>
       </CardSlider>
-    </div>
+      <Button
+        onClick={() => {
+          nextNews();
+        }}
+      >
+        <img
+          src={getRelativePath('/static/news/icon_right.svg')}
+          style={{ margin: 'auto' }}
+        />
+      </Button>
+    </CarouselContainer>
   );
 };
 
