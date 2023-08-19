9350c1b0629a64bf6edd4f068105f227a07ac3fb
Henry Hang <a54383813@gmail.com>
2023-08-19 17:37:45 +0800

    feat: get default language from user

--- assets/js/setup-i18n.js
+++ assets/js/setup-i18n.js
@@ -19,8 +19,8 @@ function onChangeLanguage() {
 }
 
 function getLanguage() {
-  var cookieArray = document.cookie.split(';');
-  var language = '';
+  let cookieArray = document.cookie.split(';');
+  let language = '';
   for (var i = 0; i < cookieArray.length; i++) {
     var cookieKey = cookieArray[i].split('=')[0];
     var cookieValue = cookieArray[i].split('=')[1];
@@ -28,13 +28,21 @@ function getLanguage() {
       language = cookieValue;
     }
   }
+
+  const defaultLang = navigator.language || navigator.userLanguage;
+  const supportLanguages = ['en', 'zh-TW', 'jp'];
+
   if (language === 'en') {
     $('#language-selector option[value="en"]').prop('selected', true);
   } else if (language === 'zh-TW') {
     $('#language-selector option[value="zh-TW"]').prop('selected', true);
-  } else {
+  } else if (language === 'jp'){
     $('#language-selector option[value="jp"]').prop('selected', true);
+  } else if (supportLanguages.includes(defaultLang)) {
+    language = defaultLang;
+    $(`#language-selector option[value="${defaultLang}"]`).prop('selected', true);
   }
+
   i18n.set("locale", language).init(function () {
     this.translate();
   });
